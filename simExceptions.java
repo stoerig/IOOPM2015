c8225f6735ba33acf7ecc14e05614f526403d027
Edvin Wahlberg <edvin.wahlberg@gmail.com>
2015-12-01 13:53:42 +0100

    la till exceptionsklassen

---
+++ simExceptions.java
@@ -0,0 +1,13 @@
+public class simExceptions extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public simExceptions(String error_message)
+	{
+		super(error_message);
+	}
+
+}
